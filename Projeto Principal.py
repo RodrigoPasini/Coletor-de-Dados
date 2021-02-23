ab71d58d8380343deb954af0270d4d99e7f099a4
felipeheuer <77160864+felipeheuer@users.noreply.github.com>
2021-02-23 13:33:01 +0100

    feat: adiciona funcao para tamanho variavel de col
    Cria uma funcao que calcula o maior tamanho para cada item em cada
    linha e salva o maior, por coluna. Depois aplica esse maio tamanho
    encontrado para fazer a largura de todas as colunas.

--- Projeto Principal.py
+++ Projeto Principal.py
@@ -13,8 +13,8 @@ while True: # LOOP START
     aluno['Idade'] = funcao.leiaIdade('Idade: ') # Key: AGE
     aluno['Rede Social'] = redesocial.socialNetwork('Você possui rede social? [S/N]')  # QUESTION ABOUT SOCIAL NETWORK
     if len(aluno['Rede Social']) != 0:
-       aluno['Grupo Rede Social'] = redesocial.groupSocialNetwork(('Você participa de algum grupo na área de TI em uma destas redes sociais? [S/N]'))  #DO YOU APLLIED IN ANYONE SOCIAL NETWORK GROUP?    
-    
+       aluno['Grupo Rede Social'] = redesocial.groupSocialNetwork(('Você participa de algum grupo na área de TI em uma destas redes sociais? [S/N]'))  #DO YOU APLLIED IN ANYONE SOCIAL NETWORK GROUP?
+
     alunos.append(aluno.copy())
     while True:
         resp = str(input('Deseja continuar? [S/N]')).upper().strip()[0] # VARIABLE TO CONTINUE OR NOT
@@ -24,32 +24,21 @@ while True: # LOOP START
     if resp == 'N':
         break
 
+tamanhos = funcao.pega_maiores_tamanhos(aluno, alunos)
+
 print('Código ', end='') # ORDER THE KEYS
 count = 0
 for i in aluno.keys():
-    if count == 4:
-        print(f'{i:<3}', end='')
-    elif count == 5:
-        print(f'{i:<6}', end='')
-    elif count == 6:
-        print(f'{i:<20}', end='')
-    else:
-        print(f'{i:<15}', end='')
+    print(f'{i:<{tamanhos[count]}}', end='')
     count += 1
 print()
+
 count = 0
 for k, v in enumerate(alunos): # ORDER THE KEYS
     count = 0
     print(f'{k+1:>6} ', end='')
     for d in v.values():
-        if count == 4:
-            print(f'{str(d):<3}', end='')
-        elif count == 5:
-            print(f'{str(d):<6}', end='')
-        elif count == 6:
-            print(f'{str(d):<20}')
-        else:
-            print(f'{str(d):<15}', end='')
+        print(f'{d:<{tamanhos[count]}}', end='')
         count += 1
     print()
 print()
